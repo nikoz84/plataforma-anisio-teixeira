46d0def7522e0aca6fea742f51c8237f7398b8cc
Fabiano Muniz <fabianomuniz.info@gmail.com>
2019-05-02 16:40:38 -0300

    aplicação do CSS na plataforma

--- resources/assets/js/components/SidebarComponent.vue
+++ resources/assets/js/components/SidebarComponent.vue
@@ -4,38 +4,38 @@
             <img class="w-150" v-bind:src="'https://via.placeholder.com/300x200'" alt="Foto Usuário">
         </a>
         <p>{{ 'Benvindo ' + username }}</p>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'inicio', action:'estatisticas' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'inicio', action:'estatisticas' }}" class="menu_admin">
             <a>Inicio</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'aplicativos', action:'listar' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'aplicativos', action:'listar' }}" class="menu_admin">
             <a>Aplicativos</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'conteudos', action:'listar' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'conteudos', action:'listar' }}" class="menu_admin">
             <a>Conteúdos</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'canais', action:'listar' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'canais', action:'listar' }}" class="menu_admin">
             <a>Canais</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'licenses', action:'listar' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'licenses', action:'listar' }}" class="menu_admin">
             <a>Licenças</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'tags', action:'listar' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'tags', action:'listar' }}" class="menu_admin">
             <a>Palavras chave</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'usuarios', action:'listar' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'usuarios', action:'listar' }}" class="menu_admin">
             <a>Usuários</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'denuncias', action:'listar' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'denuncias', action:'listar' }}" class="menu_admin">
             <a>Denúncias</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'usuarios', action:'alterar-senha' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'usuarios', action:'alterar-senha' }}" class="menu_admin">
             <a>Alterar senha</a>
         </router-link>
-        <router-link tag="li" :to=" {name:'admin', params: { slug: 'usuarios', action:'adicionar' }}">
+        <router-link tag="li" :to=" {name:'admin', params: { slug: 'usuarios', action:'adicionar' }}" class="menu_admin">
             <a>Adicionar Usuário</a>
         </router-link>
-        
-        
+
+
     </nav>
 </template>
 <script>
