0ebdeb41c417637542d636964328a52169eae899
nikoz84 <nikoz.1984@gmail.com>
2019-06-03 23:27:31 -0300

    wordpress api coneção com custom rest

--- app/Helpers/WordpressService.php
+++ app/Helpers/WordpressService.php
@@ -19,33 +19,22 @@ class WordpressService
     {
         $canal = $canal = Canal::find(7);
         $canalUrl = $canal->options['back_url'];
-        $this->slug = $canal->slug;
-        $this->api =  $canalUrl . "/wp-json/pat/v1/";
+        
+        $this->api =  $canalUrl . "wp-json/pat/v1/";
     }
 
     public function getPosts($limit = 5)
     {
         $data = [
             'per_page' => $limit,
-            '_embed' => true
+            'offset' => 1
         ];
         $url = $this->api . 'posts';
-
-        return $this->createArray($this->getData($url, $data));
-    }
-    public function getPost($request)
-    {
-        $url = $this->api . "posts/{$request->id}&_embed=true";
-
+        
         return Curl::to($url)
-            ->asJsonResponse()
-            ->get();
+        ->withData($data)
+        ->asJsonResponse()
+        ->get();
     }
-    public function getData($url = '', $data = null)
-    {
-        return Curl::to($url)
-            ->withData($data)
-            ->asJsonResponse()
-            ->get();
-    }
+    
 }
