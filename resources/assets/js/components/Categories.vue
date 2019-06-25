b23715b5209fbca89bf44693110c5848468b0b61
nikoz84 <nikoz.1984@gmail.com>
2019-06-25 18:36:40 -0300

    adicionando filtros para os conteudos digitais

--- resources/assets/js/components/Categories.vue
+++ resources/assets/js/components/Categories.vue
@@ -1,23 +1,25 @@
 <template>
     <nav role="menu categorias" v-if="categories">
-        <ul class="nav nav-pills nav-stacked">
+        <ul class="list-unstyled list-group">
+            <!-- TODOS -->
             <router-link tag="li" :to="{ name: 'Listar', params: { slug : $route.params.slug }}" class="active">
-                Todos
+                <a class="list-group-item">Todos</a>
             </router-link>
             <!-- CATEGORIAS -->
             <li v-for="(category, c) in categories" v-bind:key="c">
-                <router-link :to="{ name: 'Listar', 
-                            params: { slug : $route.params.slug }, 
-                            query: { categoria: category.id }}" exact class="categoria">
-                <a class="link">{{ category.name }}</a>
-                </router-link>
+            <router-link :to="{ name: 'Listar', 
+                        params: { slug : $route.params.slug }, 
+                        query: { categoria: category.id }}" exact>
+                <a class="list-group-item" v-text="category.name"></a>
+            </router-link>
+            
                 <!-- SUBCATEGORIAS -->
-                <ul v-if="category.sub_categories && category.sub_categories.length > 0">
-                    <li v-for="(subcategory, s) in category.sub_categories" v-bind:key="s" class="sub-categoria">
+                <ul class="list-unstyled list-group" v-if="category.sub_categories && category.sub_categories.length > 0" >
+                    <li v-for="(subcategory, s) in category.sub_categories" v-bind:key="s">
                         <router-link :to="{ name: 'Listar', 
                             params: { slug : $route.params.slug }, 
                             query: { categoria: subcategory.id }}" exact >
-                        <a>{{ subcategory.name }}</a>
+                        <a class="list-group-item" v-text="subcategory.name"></a>
                         </router-link>
                     </li>
                 </ul>
@@ -37,29 +39,5 @@ export default {
 </script>
 <style lang="scss" scoped>
 
-.categoria{
-  border: 1px solid rgb(231, 230, 230);
-  background-color: rgb(233, 233, 233);
-  margin-bottom: 2px;
-}
-
-.sub-categoria{
-  border: 1px solid rgb(231, 230, 230);
-  background-color: rgb(243, 242, 242);
-  padding: 10px 5px 5px;
-  margin-bottom: 2px;
-
-}
-
-.active{
-  background-color: rgb(131, 133, 134) !important;
-  color: #fff;
-  padding: 10px;
-  font-weight: bold;
-}
-
-ul{
-    list-style-type: none;
-}
 </style>
 
