03b121ba4f0c5d495415f90e1c88182bb7141478
valdiney <valdiney.2@hotmail.com>
2020-06-12 10:21:55 -0300

    criando migration de conteudos_planilha

---
+++ database/migrations/2020_06_12_101453_conteudos_planilha_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ConteudosPlanilhaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conteudos_planilha', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment('Chave primaria');
+            $table->string('name', 155)->comment('Nome identificador do conteudo');
+            $table->jsonb('document')->default('{}')->comment('Campo formato jsonb');
+            // campo de data created_at and updated_at
+            $table->timestamps();
+            // campo de data deleted_at
+            $table->softDeletes();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conteudos_planilha');
+    }
+}
