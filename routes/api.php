6127dd317631192ea996ca6bd754440eda684aa3
Paulo Vitor Carvalho Santos <p.vitor_8@hotmail.com>
2020-06-12 11:47:02 -0300

    resolvendo merge

--- routes/api.php
+++ routes/api.php
@@ -80,10 +80,7 @@ use Illuminate\Support\Facades\Route;
     )->name('busca.planilha.rotina.de.estudos');
 
     Route::get('/planilhas', 'ConteudoPlanilhaController@conteudos')->name('conteudo.planilha');
-<<<<<<< HEAD
-=======
     
->>>>>>> d353ee728800bfc1dfa80b7b5d7dea54ca14b5d4
 
 /**
  *
@@ -91,7 +88,6 @@ use Illuminate\Support\Facades\Route;
  * USUÁRIO DEVE ESTAR LOGADO PARA ACESSAR ESSAS ROTAS
  *
  * */
-<<<<<<< HEAD
 Route::group(
     ['middleware' => ['jwt.auth']],
     function () {
@@ -187,84 +183,3 @@ Route::group(
         )->name('gerar.relatorio.usuario');
     }
 );
-=======
-Route::group(['middleware' => ['jwt.auth']], function () {
-    /** CATEGORIAS DOS CONTEÚDOS*/
-    Route::post('/categorias', 'CategoryController@create')->name('criar.categorias');
-    Route::put('/categorias/{id}', 'CategoryController@update')->name('atualizar.categorias');
-    Route::delete('/categorias/{id}', 'CategoryController@delete')->name('categorias.apagar');
-    /** AUTENTICACAO */
-    Route::post('/auth/logout', 'AuthController@logout')->name('sair');
-    Route::post('/auth/refresh', 'AuthController@refresh')->name('refrescar.token');
-    Route::get('/auth/links-admin', 'AuthController@linksAdmin')->name('links.admin');
-    /** TIPOS */
-    Route::post('/tipos', 'TipoController@create')->name('criar.tipos');
-    Route::put('/tipos/{id}', 'TipoController@update')->name('atualizar.tipos');
-    /** ROLES */
-    Route::get('/roles', 'RoleController@index')->name('role.listar');
-    Route::post('/roles', 'RoleController@create')->name('criar.role');
-    Route::put('/roles/{id}', 'RoleController@update')->name('atualizar.role');
-    Route::delete('/roles/{id}', 'RoleController@delete')->name('deletar.role');
-    Route::get('/roles/search/{term}', 'RoleController@search')->name('busca.role');
-    /** USUARIOS */
-    Route::get('/usuarios/search/{termo}', 'UserController@search')->name('usuario.buscar');
-    Route::delete('/usuarios/{id}', 'UserController@delete')->name('usuario.apagar');
-    Route::get('/usuarios/{id}', 'UserController@getById')->name('user.x.id');
-    Route::get('/usuarios', 'UserController@index')->name('usuario.listar');
-    Route::put('/usuarios/{id}', 'UserController@update');
-    Route::put('/usuarios/reset-password', 'UserController@resetPass')->name('senha.atualizar');
-    Route::post('/usuarios', 'UserController@create')->name('adicionar.usuario');
-    /** APLICATIVOS */
-    Route::post('/aplicativos', 'AplicativoController@create')->name('aplicativo.adicionar');
-    Route::put('/aplicativos/{id}', 'AplicativoController@update')->name('aplicativo.editar');
-    Route::delete('/aplicativos/{id}', 'AplicativoController@delete')->name('aplicativo.apagar');
-    /** APLICATIVOS CATEGORIES */
-    Route::post('/aplicativos/categories', 'AplicativoCategoryController@create')->name('criar.aplicativo.categorias');
-    Route::put('/aplicativos/categories/{id}', 'AplicativoCategoryController@update')
-        ->name('atualizar.aplicativo.categorias');
-    Route::delete('/aplicativos/categories/{id}', 'AplicativoCategoryController@delete')
-        ->name('apagar.aplicativo.categorias');
-    /** TAGS */
-    Route::get('/tags', 'TagController@index')->name('lista.tag');
-    Route::post('/tags', 'TagController@create')->name('adicionar.tag');
-    Route::get('/tags/search/{term}', 'TagController@search')->name('busca.tag');
-    Route::get('/tags/autocomplete/{term}', 'TagController@autocomplete')->name('autocompletar.tag');
-    Route::put('/tags/{id}', 'TagController@update')->name('atualizar.tag');
-    Route::delete('/tags/{id}', 'TagController@delete')->name('apagar.tag');
-    /** CONTEUDOS */
-    Route::post('/conteudos', 'ConteudoController@create')->name('adicionar.conteudo');
-    Route::put('/conteudos/{id}', 'ConteudoController@update')->name('atualizar.conteudo');
-    Route::delete('/conteudos/{id}', 'ConteudoController@delete')->name('apagar.conteudo');
-    Route::post('/conteudos/arquivos', 'ConteudoController@storeFiles')->name('salvar.arquivo.conteudo');
-    /** CANAIS */
-    Route::get('/canais', 'CanalController@index')->name('listar.canais');
-    Route::post('/canais', 'CanalController@create')->name('adicionar.canal');
-    Route::put('/canais/{id}', 'CanalController@update')
-        ->name('atualizar.canal')
-        ->middleware('can:update,canal');
-    Route::delete('/canais/{id}', 'CanalController@delete')->name('apagar.canal');
-    Route::get('/canais/{id}', 'CanalController@getById')->name('listar.canal.x.id');
-    Route::get('/canais/search/{term}', 'CanalController@search')->name('buscar.canal');
-    /** LICENCAS */
-    Route::get('/licencas/search/{term}', 'LicenseController@search')->name('buscar.licenca');
-    Route::post('/licencas', 'LicenseController@create')->name('adicionar.licenca');
-    Route::put('/licencas/{id}', 'LicenseController@update')->name('atualizar.licenca');
-    Route::delete('/licencas/{id}', 'LicenseController@delete')->name('apagar.licenca');
-    /** DENUNCIAS */
-    Route::get('/contato', 'ContatoController@index')->name('listar.faleconosco');
-    Route::get('/contato/{id}', 'ContatoController@getById')->name('busca.x.id');
-    Route::delete('/contato/{id}', 'ContatoController@delete')->name('apagar.contato');
-    /** OPTIONS */
-    Route::post('/options', 'OptionsController@create')->name('criar.opcoes');
-    Route::put('/options/{name}', 'OptionsController@update')->name('atualizar.opcoes.x.nome');
-    Route::delete('/options/{name}', 'OptionsController@delete')->name('apagar.opcoes.x.nome');
-    Route::post('/options/destaques/', 'OptionsController@createDestaques')->name('cria.destaques');
-    Route::get('/options/id/{id}', 'OptionsController@getById')->name('opcao.x.id');
-    /** ANALYTICS */
-    Route::get('/analytics', 'HomeController@getAnalytics')->name('catalogacao.blog.e.plataforma');
-    /** RELATÓRIOS */
-    Route::get('/usuarios/role/{role_id}', 'RelatorioController@buscarUsuariosPorRole')->name('view.relatorio.usuario');
-    Route::get('/relatorio/conteudos/{flag}', 'RelatorioController@gerarPdfConteudo')->name('gerar.relatorio.conteudo');
-    Route::get('/relatorio/usuarios/role/{role_id}/{is_active?}', 'RelatorioController@gerarPdfUsuario')->name('gerar.relatorio.usuario');
-});
->>>>>>> d353ee728800bfc1dfa80b7b5d7dea54ca14b5d4
