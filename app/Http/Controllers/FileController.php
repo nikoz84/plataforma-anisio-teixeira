4cb34a3e61461fb2f2bd03fbdccdbebf3d6c6ba7
Paulo Vitor Carvalho Santos <p.vitor_8@hotmail.com>
2020-07-02 16:25:07 -0300

    funcionalidade de conversão de PDF para Imagem, com Imagick e ghostscript

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -279,3 +279,30 @@ class FileController extends ApiController
         return $path ? $path : $this->errorResponse([], "Falha ao fazer upload!", 500);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Link tutorial instalação e integração imagick ao php no windows:
+https://ourcodeworld.com/articles/read/349/how-to-install-and-enable-the-imagick-extension-in-xampp-for-windows
+
+Ferramentas necessária para instalação imagick:
+
+#1 Image Magick:     https://www.imagemagick.org/script/download.php
+
+#2 Imagick for PHP:  https://pecl.php.net/package/imagick
+
+#3 Imagick Binaries: https://windows.php.net/downloads/pecl/deps/
+
+#install ghostscript:https://www.ghostscript.com/download/gsdnld.html
+ */
