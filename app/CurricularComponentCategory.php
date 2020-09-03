159b396369ef39af270d9a173dc7a813bf234024
Nicolás Romero <nikoz.1984@gmail.com>
2020-09-03 06:22:06 -0300

    mudança em categoria componente curricular, adiciono protected  = ['name']

--- app/CurricularComponentCategory.php
+++ app/CurricularComponentCategory.php
@@ -11,6 +11,7 @@ class CurricularComponentCategory extends Model
     use UserCan;
 
     protected $table = 'curricular_components_categories';
+    protected $fillable = ['name'];
     protected $appends = ['user_can', 'search_url'];
      
     /**
