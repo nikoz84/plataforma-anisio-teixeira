7142e895c85c3dc71300f7737873ce9acdd824b9
alexandreleao <erdleosalgueiro@gmail.com>
2022-04-20 14:44:16 -0300

    Niveis ensino cadastrando

--- resources/assets/js/forms/models/NivelEnsinoForm.vue
+++ resources/assets/js/forms/models/NivelEnsinoForm.vue
@@ -93,17 +93,20 @@ export default {
             }
             try
             {
-                if (this.$route.params.action == "editar") 
-                {
-                    form.append("id", this.nivelEnsino.id);    
-                    form.append("_method", "PUT");
-                    let resp = await axios.post(this.$route.params.slug +"/"+ this.nivelEnsino.id, form);
-                }
-                else
-                {
-                    let resp = await axios.post(this.$route.params.slug , form);
-                }
-                this.$router.push(`/admin/nivelensino/listar`);
+               let url = '/niveis-ensino'
+
+               if(this.$route.params.id){
+                   form.append("id", this.nivelEnsino.id);
+                   form.append("_method", "PUT");
+                   url = url + `/${this.$route.params.id}`
+               }
+
+               const resp = await axios.post(url, form);
+
+               if(resp.status === 200){
+                   this.$router.push(`/admin/niveis-ensino/listar`)
+               }
+                
             }
             catch(ex)
             {
@@ -118,7 +121,7 @@ export default {
                 this.componentes = [];
                 } else {
                 const self = this;
-                axios.get(`componentes/autocomplete/${val}`).then(resp => {
+                axios.get(`niveis-ensino/autocomplete/${val}`).then(resp => {
                     self.componentes = resp.data.metadata;
                 });
                 }
