12aac54b832e40ddaa9430435fbd43f50c063e77
Alexandre <erdleosalgueiro@gmail.com>
2022-05-13 10:49:26 -0300

    Criação de pasta Wordpres dentro da models e arquivo Post.php

---
+++ app/Models/Wordpress/Post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Wordpress;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $connection = 'mysql';
+}
