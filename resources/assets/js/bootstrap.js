c3ac2cb0a56e1afb391093921da322089046fabb
Shift <shift@laravelshift.com>
2022-07-13 03:51:54 +0000

    Rename ENV variables

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -46,7 +46,7 @@ Vue.use(Quasar, {
 import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.baseURL = process.env.MIX_API_URL;
+window.axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
