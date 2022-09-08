c9671219e8cbcaaafc8e2ec7ba81c715dac110d3
davidgalvao <mrdavidalmeida@gmail.com>
2022-09-08 11:57:59 -0300

    layout dashboard e detalhes de relatório

--- resources/assets/js/components/dashboard/CardDashboard.vue
+++ resources/assets/js/components/dashboard/CardDashboard.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="q-pa-md row items-start q-gutter-md">
-        <q-card class="my-card text-white" style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
-            <q-card-section>
-                <div class=" text-h6">
-                    {{ data.titulo }}
-                </div>
-                <q-card-section horizontal>
-                    <q-card-section>
-                        Quantidade: {{ data.quantidade }}
-                    </q-card-section>
-                </q-card-section>
-            </q-card-section>
-            <q-card-section>
-                <q-btn label="Ver mais" @click="goto(id)"></q-btn>
-            </q-card-section>
-        </q-card>
-    </div>
+  <div class="row items-start">
+    <q-card class="my-card text-dark" style="" square>
+      <!-- TÍTULO DOS DADOS -->
+      <q-card-section>
+        <div class="text-h6">
+          {{ data.titulo }}
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <!-- ENTRADA DE DADOS -->
+      <q-card-section> Quantidade: {{ data.quantidade }} </q-card-section>
+
+      <q-separator />
+
+      <!-- AÇÕES DOS DADOS -->
+      <q-card-actions>
+        <q-btn flat @click="goto(id)" size="sm">Ver relatório completo</q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CardDashboard',
-    props: [
-        'id',
-        'data'
-    ],
-    methods: {
-        goto (ID) {
-            this.$router.push('/admin/dashboard/listar/' + ID)
-        }
-    }
-
-}
+  name: "CardDashboard",
+  props: ["id", "data"],
+  methods: {
+    goto(ID) {
+      this.$router.push("/admin/dashboard/listar/" + ID);
+    },
+  },
+};
 </script>
 <style scoped>
 .my-card {
-    width: 100%;
-    max-width: 250px;
-    border-radius: 15px
+  width: 450px;
+  max-width: 450px;
 }
 </style>
--- resources/assets/js/pages/Dashboard.vue
+++ resources/assets/js/pages/Dashboard.vue
@@ -1,22 +1,85 @@
 <template>
   <div class="q-pa-md">
-    <q-select filled v-model="item" use-input option-value="value" option-label="label" use-chips stack-value
-      input-debounce="0" :options="selectOptions" class="q-mb-lg" @input="getData" />
-    <div class="q-pa-md row items-start q-gutter-md">
-      <card-dashboard v-for="(item, i) in dashboardData" :key="i" :data="item.data" :id="item.id" />
+    <div class="q-mb-md">
+      <div class="text-h5 title-page text-primary separatriz q-pb-md">
+        Dashboard
+      </div>
+      <div class="separatriz-6"></div>
     </div>
-    <hr>
+    <div class="row items-start q-gutter-md q-mb-xl">
+      <card-dashboard
+        v-for="(item, i) in dashboardData"
+        :key="i"
+        :data="item.data"
+        :id="item.id"
+      />
+    </div>
+    <q-select
+      filled
+      v-model="item"
+      use-input
+      option-value="value"
+      option-label="label"
+      use-chips
+      stack-value
+      input-debounce="0"
+      :options="selectOptions"
+      class="q-mb-lg"
+      @input="getData"
+    />
+    <hr />
     <q-card>
       <q-card-section>
-        <VueApexCharts height="450" v-if="render" type="bar" :options="chartOptions" :series="series"></VueApexCharts>
+        <div class="text-dark text-h6">Filtros</div>
+        <div class="q-gutter-md row items-start">
+          <div style="min-width: 250px; max-width: 300px">
+            <q-select
+              v-model="anoMultiple"
+              multiple
+              label-color="primary"
+              :options="anos"
+              use-chips
+              stack-label
+              label="Anos"
+            />
+          </div>
+          <div style="min-width: 250px; max-width: 300px">
+            <q-select
+              v-model="temaMultiple"
+              multiple
+              label-color="primary"
+              :options="temas"
+              use-chips
+              stack-label
+              label="Tema ou Disciplina"
+            />
+          </div>
+        </div>
+      </q-card-section>
+      <q-separator />
+      <q-card-section>
+        <VueApexCharts
+          height="450"
+          v-if="render"
+          type="bar"
+          :options="chartOptions"
+          :series="series"
+        ></VueApexCharts>
       </q-card-section>
       <q-card-section>
-        <q-table :title="title" :data="metadata" :columns="columns" row-key="id" separator="horizontal" />
+        <q-table
+          :title="title"
+          :data="metadata"
+          :columns="columns"
+          row-key="id"
+          separator="horizontal"
+        />
       </q-card-section>
     </q-card>
   </div>
 </template>
-<script>// @ts-nocheck
+<script>
+// @ts-nocheck
 
 import VueApexCharts from "vue-apexcharts";
 import {
@@ -25,7 +88,7 @@ import {
   QCardSection,
   QSeparator,
   QSelect,
-  QChip
+  QChip,
 } from "quasar";
 import { CardDashboard } from "@components/dashboard";
 
@@ -39,9 +102,9 @@ export default {
     QCardSection,
     QSeparator,
     VueApexCharts,
-    CardDashboard
+    CardDashboard,
   },
-  data () {
+  data() {
     return {
       title: "",
       render: false,
@@ -51,19 +114,19 @@ export default {
         {
           name: "name",
           label: "Nome",
-          field: "name"
+          field: "name",
         },
         {
           name: "total",
           label: "Total",
-          field: "total"
+          field: "total",
         },
         {
           name: "month",
           label: "Mês",
           field: "month",
-          required: false
-        }
+          required: false,
+        },
       ],
       item: { label: "Catalogação por usuário", value: "per_user" },
       selectOptions: [
@@ -71,7 +134,7 @@ export default {
         { label: "Catalogação por usuário", value: "per_user" },
         {
           label: "Catalogação por canal",
-          value: "per_chanel"
+          value: "per_chanel",
         },
         { label: "Catalogação mensal por usuário", value: "user_montly" },
         { label: "Catalogação total mensal", value: "per_month" },
@@ -79,43 +142,56 @@ export default {
         { label: "Conteúdos mais baixados", value: "qt_downloads" },
         { label: "Conteúdos mais acessados", value: "qt_access" },
         { label: "Tags mais procuradas", value: "searched_tags" },
-        { label: "Aplicativos mais visualizados", value: "aplicativo_qt_access" },
-        { label: "Registro de ocorrência formulário de contatos", value: "registro_mes_ocorrencia" },
-        { label: "Conteúdos mais acessados nos últimos três meses", value: "acessados_ultimos_meses" },
-        { label: "Catalogação BLOG", value: "wordpress_data" }
+        {
+          label: "Aplicativos mais visualizados",
+          value: "aplicativo_qt_access",
+        },
+        {
+          label: "Registro de ocorrência formulário de contatos",
+          value: "registro_mes_ocorrencia",
+        },
+        {
+          label: "Conteúdos mais acessados nos últimos três meses",
+          value: "acessados_ultimos_meses",
+        },
+        { label: "Catalogação BLOG", value: "wordpress_data" },
       ],
       chartOptions: {
         chart: {
           id: "vuechart-teste",
           height: 430,
           width: "100%",
-          type: "bar"
+          type: "bar",
         },
         title: {
           text: "hola",
-          align: "center"
+          align: "center",
         },
         plotOptions: {
           bar: {
-            horizontal: true
-          }
+            horizontal: true,
+          },
         },
         dataLabels: {
-          enabled: false
+          enabled: false,
         },
         xaxis: {
-          categories: []
-        }
+          categories: [],
+        },
       },
-      series: [{ name: "Quantidade", data: [] }]
+      series: [{ name: "Quantidade", data: [] }],
+      anoMultiple: null,
+      temaMultiple: null,
+      anos: ["Google", "Facebook", "Twitter", "Apple", "Oracle"],
+      temas: ["123", "456", "789", "000"],
     };
   },
-  created () {
+  created() {
     this.getData();
     this.getDashboardData();
   },
   methods: {
-    async getData () {
+    async getData() {
       let url = `resumo?option=${this.item.value}`;
       let resp = await axios.get(url);
       if (resp.data.success) {
@@ -129,38 +205,37 @@ export default {
       }
     },
 
-    async getDashboardData () {
+    async getDashboardData() {
+      let inicio = "01-01-2021";
+      let fim = "02-02-2021";
 
-      let inicio = '01-01-2021';
-      let fim = '02-02-2021';
-
-      const { data } = await axios.get(`/dashboard?inicio=${inicio}&fim=${fim}`);
-      console.log(data)
+      const { data } = await axios.get(
+        `/dashboard?inicio=${inicio}&fim=${fim}`
+      );
+      console.log(data);
       if (data.success) {
-        this.dashboardData = data.metadata
+        this.dashboardData = data.metadata;
       }
-
     },
 
-
-    createInfoGraf (categories) {
+    createInfoGraf(categories) {
       this.chartOptions = {
         ...this.chartOptions,
         ...{
           title: {
-            text: this.title
+            text: this.title,
           },
           xaxis: {
-            categories
-          }
-        }
+            categories,
+          },
+        },
       };
     },
-    appendData (data) {
+    appendData(data) {
       let arr = this.series.slice();
       arr[0].data = data;
       this.series = arr;
-    }
-  }
+    },
+  },
 };
 </script>
