1f1628414501d64b49d0b5de3478073662665a7e
reinaldodev <reinaldoweb@hotmail.com>
2022-10-17 13:51:28 -0300

    configuranção do grafico catalogação total mensal

--- resources/assets/js/components/dashboard/CatalogacaoTotalMensal.vue
+++ resources/assets/js/components/dashboard/CatalogacaoTotalMensal.vue
@@ -4,120 +4,66 @@
             <q-card>
                 <q-separator />
                 <q-card-section>
-                    <div class="text-dark text-h6">Filtros</div>
-                    <div class="q-gutter-md row items-start">
-                        <div style="min-width: 250px; max-width: 300px">
-                            <q-select v-model="mesMultiple" multiple label-color="primary" :options="meses" use-chips
-                                stack-label label="Filtrar por meses" />
-                        </div>
-                        <div style="min-width: 250px; max-width: 300px">
-                            <q-select v-model="anoMultiple" multiple label-color="primary" :options="anos" use-chips
-                                stack-label label="Filtrar por anos" />
-                        </div>
-                        <div style="min-width: 250px; max-width: 300px">
-                            <q-select v-model="temaMultiple" multiple label-color="primary" :options="temas" use-chips
-                                stack-label label="Tema ou Disciplina" />
-                        </div>
-                        <div style="min-width: 250px; max-width: 300px">
-                            <q-select v-model="tipoConteudoMultiple" multiple label-color="primary"
-                                :options="tipoConteudo" use-chips stack-label label="Tipo de conteúdo" />
-                        </div>
-                        <div style="min-width: 250px; max-width: 300px">
-                            <q-select v-model="ordenarMultiple" label-color="primary" :options="ordenar" use-chips
-                                stack-label label="Ordenar por:" />
-                        </div>
-                    </div>
+
                 </q-card-section>
-                <q-table title="Catalogação Total Mensal" :data="data" :columns="columns" color="primary"
-                    row-key="name">
+                <q-table v-if="render" title="Catalogação total mensal" :data="dataTable" :columns="columns"
+                    style="width: 95%" color="primary" row-key="name" :pagination="{ rowsPerPage: 20 }">
                     <template v-slot:top-right>
                         <q-btn color="primary" icon-right="archive" label="Export to csv" no-caps
-                            @click="exportTable" />
+                            @click="exportToCsv" />
                     </template>
+
                 </q-table>
             </q-card>
         </q-section>
         <q-separator />
-        <q-card-section>
-            <VueApexCharts height="450" v-if="render" type="bar" :options="chartOptions" :series="series" />
-
+        <q-card-section v-if="render">
+            <VueApexCharts height="450" type="line" :options="chartOptions" :series="mapSeries" />
         </q-card-section>
     </div>
 </template>
 
 <script>
-import { exportFile } from 'quasar';
+import { exportTable } from "@composables/ToCsv";
 import VueApexCharts from "vue-apexcharts";
 
 export default {
-    name: 'CatalogacaoTotalMensal',
+    name: "CatalogacaoTotalMensal",
     components: {
-        VueApexCharts
+        VueApexCharts,
     },
     data () {
         return {
             columns: [
+                {
+                    name: "mes",
+                    align: "left",
+                    label: "Mês",
+                    field: "mes",
+                    sortable: true,
+                    width: "100%"
 
-                { name: 'Titulo', align: 'center', label: 'Títulos', field: 'titulos' },
-                { name: 'Descricao', label: 'Descrição', field: 'descricao' },
-                { name: 'Author', label: 'Autor', field: 'autor' },
-                { name: 'Q.Downloads', label: 'Q.Downloads', field: 'qt_downloads' },
-                { name: 'Qt.Acessos', label: 'Qt.Acessos', field: 'qt_access' },
-                { name: 'Dt.Criacao', label: 'Dt.Criação', field: 'created_at' }
-
+                },
+                {
+                    name: "Quantidade",
+                    label: "Quantidade",
+                    field: "quantidade"
+                },
             ],
-
-            series: [{ name: "Quantidade", data: [] }],
-            mesMultiple: null,
-            anoMultiple: null,
-            temaMultiple: null,
-            tipoConteudoMultiple: null,
-            ordenarMultiple: null,
-            meses: [
-                "Janeiro",
-                "Fevereiro",
-                "Março",
-                "Abril",
-                "Maio",
-                "Junho",
-                "Julho",
-                "Agosto",
-                "Setembro",
-                "Outubro",
-                "Novembro",
-                "Dezembro",
-            ],
-            anos: ["2022", "2021", "2020", "2019", "2018"],
-            temas: ["Matemática", "Física", "Química", "Sexualidade"],
-            tipoConteudo: [
-                "Videos",
-                "Apresentações",
-                "Áudios",
-                "Aplicativos",
-                "Games",
-            ],
-            ordenar: [
-                "Mais baixados",
-                "Mais visualizados",
-                "Mais acessados",
-                "Catalogado por usuário",
-            ],
+            dataTable: [],
+            mapSeries: [],
+            render: false,
+            // Inicio da configuração do gráfico
             chartOptions: {
                 chart: {
-                    id: "vuechart-teste",
-                    height: 430,
-                    width: "100%",
-                    type: "bar",
+                    height: 350,
+                    type: 'area'
                 },
                 title: {
-                    text: "hola",
+                    text: "Catalogação Total mensal",
                     align: "center",
                 },
-                plotOptions: {
-                    bar: {
-                        horizontal: true,
-                    },
-                },
+
                 dataLabels: {
                     enabled: false,
                 },
@@ -125,11 +71,46 @@ export default {
                     categories: [],
                 },
             },
-
         }
-    }
-}
+    },
+    created () {
+        this.getDataTable();
+    },
+    methods: {
+        exportToCsv () {
+            exportTable(this.dataTable, this.columns);
+        },
+
+        async getDataTable () {
+            this.$q.loading.show();
+            const { data } = await axios.get(`/dashboard/catalogacao-total-mensal`);
+            if (data.success) {
+                this.dataTable = data.metadata;
+                // define as as cetegorias com o spread operator (...)
+                this.chartOptions = {
+                    ...this.chartOptions,
+                    ...{
+                        xaxis: {
+                            categories: data.metadata.map((item) => item.mes),
+                        },
+                    },
+                };
+                // define as series
+                this.mapSeries = [
+                    {
+                        name: "Download",
+                        data: data.metadata.map((item) => item.quantidade),
+                    },
+                ];
+                // renderiza
+                this.render = data.success;
+            }
+            this.$q.loading.hide();
+        },
+    },
+};
 </script>
 <style scoped>
 
 </style>
+
