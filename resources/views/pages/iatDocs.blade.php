5b5aea99b88dd675e777c08496dd808679ab92dc
nikoz84 <nicolas.hernandez@dasa.com.br>
2023-01-05 18:07:16 -0300

    add style

--- resources/views/pages/iatDocs.blade.php
+++ resources/views/pages/iatDocs.blade.php
@@ -11,33 +11,35 @@
 </head>
 
 <body>
-    <h5>Listar todos os arquivos da para raiz {{ $diretorio->folder_name }}</h5>
-    <ul>
-        @foreach ($diretorio->files as $file)
-            <li><a href="{{ $file->get('url') }}" download> {{ $file->get('name') }} </a> - {{ $file->get('size') }}
-            </li>
-        @endforeach
-    </ul>
+    <section class="container">
+        <h5 class="mt-5">Listar todos os arquivos da para raiz {{ $diretorio->folder_name }}</h5>
+        <ul>
+            @foreach ($diretorio->files as $file)
+                <li><a href="{{ $file->get('url') }}" download> {{ $file->get('name') }} </a> - {{ $file->get('size') }}
+                </li>
+            @endforeach
+        </ul>
 
-    <h5>Lista de subdiretorios</h5>
-    <ul>
-        @foreach ($subdiretorios as $subdiretorio)
-            <li>
-                {{ $subdiretorio->folder_name }}
-                <ul>
-                    @foreach ($subdiretorio->files as $file)
-                        <li>
-                            <a href="{{ $file->get('url') }}" download> {{ $file->get('name') }} </a> -
-                            {{ $file->get('size') }}
-                        </li>
-                    @endforeach
-                </ul>
+        <h5>Lista de subdiretorios</h5>
+        <ul>
+            @foreach ($subdiretorios as $subdiretorio)
+                <li>
+                    {{ $subdiretorio->folder_name }}
+                    <ul>
+                        @foreach ($subdiretorio->files as $file)
+                            <li>
+                                <a href="{{ $file->get('url') }}" download> {{ $file->get('name') }} </a> -
+                                {{ $file->get('size') }}
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            @endforeach
             </li>
-        @endforeach
-        </li>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-        </script>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
