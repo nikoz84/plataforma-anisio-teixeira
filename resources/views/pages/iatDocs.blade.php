721a1e55f2431eca1e2fe93ba83ba7a847174a90
nikoz84 <nicolas.hernandez@dasa.com.br>
2023-01-05 18:01:41 -0300

    add bootstrap

--- resources/views/pages/iatDocs.blade.php
+++ resources/views/pages/iatDocs.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Documentos da Diretória</title>
 </head>
 
@@ -12,7 +14,8 @@
     <h5>Listar todos os arquivos da para raiz {{ $diretorio->folder_name }}</h5>
     <ul>
         @foreach ($diretorio->files as $file)
-            <li><a href="{{ $file->get('url') }}" download> {{ $file->get('name') }} </a> - {{ $file->get('size') }}</li>
+            <li><a href="{{ $file->get('url') }}" download> {{ $file->get('name') }} </a> - {{ $file->get('size') }}
+            </li>
         @endforeach
     </ul>
 
@@ -32,14 +35,9 @@
             </li>
         @endforeach
         </li>
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
-<script>
-    function copiarCaminho(indice) {
-        var input = document.getElementById('caminho-' + indice);
-        input.select();
-        document.execCommand('copy');
-    }
-</script>
